214d05bcee4c8bb22df6a798869f130bae35b36d
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-07 17:35:50 +0200

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -14,14 +14,25 @@ public class Hello {
 
         }
 
-        int secondTopScore = 81;
+        int secondTopScore = 95;
         if ((topScore > secondTopScore && topScore < 100)) {
             System.out.println("Greater than second top score and less than 100");
         }
 
- //   ||  //
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
 
-        
+       int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is an error");
+
+        }
+        boolean isCar = false;
+        if (isCar == true){
+            System.out.println("This is not supposed to happen");
+        }
+
 
     }
 }
