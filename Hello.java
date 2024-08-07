a04040c820640808359a90db10fec558cb54131d
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-07 21:43:57 +0200

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -35,7 +35,11 @@ public class Hello {
 
         String makeOfCar = "Volkswagen";
         boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
-        
+        if (isDomestic) {
+            System.out.println("This car is domestic to our country");
+        }
+            String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+        System.out.println(s);
 
 
 
