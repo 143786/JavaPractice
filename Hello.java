96eb6a7b3d586b145ac8e399b71a2f3e3ecfa269
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-07 22:47:14 +0200

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -45,7 +45,7 @@ public class Hello {
 
         double myFirstValue = 20.00d;
         double mySecondValue = 80.00d;
-        double myValuesTotal = myFirstValue + mySecondValue * 100.00d;
+        double myValuesTotal = ( myFirstValue + mySecondValue ) * 100.00d;
         System.out.println("myValuesTotal = " + myValuesTotal);
         double theRemainder = myValuesTotal % 40.00d;
         System.out.println("theRemainder = " + theRemainder);
