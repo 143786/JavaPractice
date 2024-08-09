dd4b8916d321feac76cf26f7e218ca79cbea66e3
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-09 15:01:59 +0200

    Add files via upload

--- RandomNumbersMain.java
+++ RandomNumbersMain.java
@@ -3,9 +3,11 @@ public class RandomNumbersMain {
     public static void main(String[] args) {
 
         Random random = new Random();
-        int x = random.nextInt(20);
+        // int x = random.nextInt(6)+1;
+        // double y = random.nextDouble();
+        boolean z = random.nextBoolean();
 
-        System.out.println(x);
+        System.out.println(z);
 
 
     }
