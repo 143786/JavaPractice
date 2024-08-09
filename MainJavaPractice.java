7265bc6fa2bde385231bf8eac13efb2478496969
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-09 09:59:18 +0200

    Add files via upload

---
+++ MainJavaPractice.java
@@ -0,0 +1,9 @@
+public class MainJavaPractice {
+    public static void main(String[] args) {
+       String x = "water";
+       String y = " kool-Aid";
+
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
+    }
+}
