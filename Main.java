281238e299e156757df81c99ef68dd848ae20ff1
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-09 12:11:10 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,13 @@
+import javax.swing.JOptionPane;
+
 public class Main {
-
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
 
-        if (score < 5000 && score > 1000 ) {
-            System.out.println("Your score was less than 5000 but greater than 1000");
-        }  else if (score < 1000 ) {
-                System.out.println("Your score was less than 1000");
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello "+name);
 
-        } else {
-            System.out.println("Got here");
-        }
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
 
     }
 }
