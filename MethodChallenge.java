8f77c8a85555e113988a128f64854b5e9c3fc765
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-09 17:39:31 +0200

    Add files via upload

--- MethodChallenge.java
+++ MethodChallenge.java
@@ -1,10 +1,27 @@
 public class MethodChallenge {
 
     public static void main(String[] args) {
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Tim", highScorePosition);
+
+         highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Ayline", highScorePosition);
+
+         highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Nil", highScorePosition);
+
+         highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Tabish", highScorePosition);
+
+         highScorePosition = calculateHighScorePosition(25);
+        displayHighScorePosition("Sean", highScorePosition);
     }
+    
     public static void displayHighScorePosition(String playerName, int highScorePosition){
         System.out.println(playerName + " managed to get into position "
         + highScorePosition + " on the high score list");
+
+
 
     }
 
