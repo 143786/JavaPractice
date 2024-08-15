89a08b938850c6e0b14336ff8d04a2bb6320bfd0
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-16 01:27:08 +0200

    Add files via upload

---
+++ whileLoop.java
@@ -0,0 +1,6 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        /* while loop =  execute a block of code as
+         long as  it 's condition remains true */
+    }
+}
