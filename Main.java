20186322a957a89b1387fe137698e6eef7f7b6ce
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-15 12:09:58 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         // if statement =  performs a block of code if it's condition evaluates to be here
-        int age = 
+        int age = 8;
+
+        if(age>= 18){
+            System.out.println("You are an adult!");
+
+        }
+        else {
+            System.out.println("You are not an adult");
+        }
+
     }
 }
