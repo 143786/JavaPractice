f8cc5bf988553f10708b155eb91aaed5fa0d9350
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-16 01:22:43 +0200

    Add files via upload

--- LogicalOperators.java
+++ LogicalOperators.java
@@ -21,8 +21,7 @@ public class LogicalOperators {
             System.out.println("It is cold Outside");
         }
 */
-
-        Scanner scanner = new Scanner(System.in);
+        /*  Scanner scanner = new Scanner(System.in);
         System.out.println("You are playing a game! Press q or Q to quit");
         String response = scanner.next();
 
@@ -30,6 +29,19 @@ public class LogicalOperators {
             System.out.println("You quit the game");
         } else {
             System.out.println("You are still playing the game *pew pew*");
+        }
+        */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("You are playing a game! Press q or Q to quit");
+        String response = scanner.next();
+
+        if (!response.equals("q") && !response.equals("Q")) {
+            System.out.println("You are still playing the game *pew pew*");
+
+        }
+        else {
+            System.out.println("You quit the game");
         }
 
     }
