7151f77e57b6feffb952d218018d11bf09f41dc9
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-18 10:15:23 +0200

    Add files via upload

--- ArithmaticOperations.java
+++ ArithmaticOperations.java
@@ -2,8 +2,14 @@ public class ArithmaticOperations {
     public static void main(String[] args) {
 
         int num1 = 100;
-        int unm2 = 200;
-        System.out.println("Addition : " + (num1 + unm2));
+        int num2 = 200;
+        System.out.println("Addition : " + (num1 + num2));
+        System.out.println("Subtraction : " + (num1 - num2));
+        System.out.println("Multiplication : " + (num1 * num2));
+        System.out.println("Division : " + (num2 / num1));
+        System.out.println("Remainder :" + (num2 % num1));
+
     }
-    
+
 }
+ 
