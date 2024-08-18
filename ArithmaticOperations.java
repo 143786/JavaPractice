a630b3d4f6ff52b64fcf9d7dddcc7b7fbd6db63b
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-18 09:23:36 +0200

    Add files via upload

---
+++ ArithmaticOperations.java
@@ -0,0 +1,9 @@
+public class ArithmaticOperations {
+    public static void main(String[] args) {
+
+        int num1 = 100;
+        int unm2 = 200;
+        System.out.println("Addition : " + (num1 + unm2));
+    }
+    
+}
