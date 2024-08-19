6df720db0f3c66bc70fb8fdaa06bbd34a699f3f5
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-19 23:24:37 +0200

    Add files via upload

--- TernaryOperator.java
+++ TernaryOperator.java
@@ -1,11 +1,14 @@
 public class TernaryOperator {
     public static void main(String[] args) {
 
-        boolean isAuthenticated = false;
+        boolean isAuthenticated = true;
         if(isAuthenticated)
             System.out.println("You are logged-in successfully.");
         else
             System.out.println("You are not logged-in.");
+
+        String result = (isAuthenticated) ? "LoggedIn" : "Not LoggedIn";
+        System.out.println(result);
 
     }
 }
