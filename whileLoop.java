f31039d05423f26222475dfc3cad38c10f5023ea
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-20 15:39:41 +0200

    Add files via upload

--- whileLoop.java
+++ whileLoop.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class whileLoop {
     public static void main(String[] args) {
         /* while loop =  execute a block of code as
          long as  it 's condition remains true */
+
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+
+        while (name.isBlank()) {
+            System.out.println("Enter your name: ");
+            name = scanner.nextLine();
+
+        }
+        System.out.println("Hello "+name);
+
     }
 }
