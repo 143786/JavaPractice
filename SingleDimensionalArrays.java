b9e4fd498551b9e13245d9a5d9bc75defdc6b80f
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-20 11:00:40 +0200

    Add files via upload

---
+++ SingleDimensionalArrays.java
@@ -0,0 +1,25 @@
+public class SingleDimensionalArrays {
+    public static void main(String[] args) {
+        // Declaring an array
+        // int [] marks;
+       // marks = new int[5];
+
+     //   int [] marks = new int[5];
+
+        // initializing an array:
+         // int[] marks = new int[] {10, 20, 30, 40, 50};
+
+        // Declare and Assign
+            int[] marks = new int[5];
+            marks[0] = 100;
+            marks[1] = 60;
+            marks[2] = 78;
+            marks[3] = 80;
+            marks[4] = 98;
+        for (int i = 0; i < marks.length; i++) {
+            
+            
+        }
+
+    }
+}
