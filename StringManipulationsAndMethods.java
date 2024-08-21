d9569dd125d4afc500ffe49177e305f6a42a587d
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-21 23:47:23 +0200

    Add files via upload

--- StringManipulationsAndMethods.java
+++ StringManipulationsAndMethods.java
@@ -1,2 +1,17 @@
 public class StringManipulationsAndMethods {
+    public static void main(String[] args) {
+
+        // Using String literal
+        String str1 = "Hello ";
+        System.out.println(str1);
+
+        // Using string Object
+        String str2 = new String("World");
+        System.out.println(str2);
+
+        // Concatenating String Literal and String Object
+        String str3 = str1 + str2;
+        System.out.println(str3);
+
+    }
 }
