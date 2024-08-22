745e08cf6115c95eceadeb1404660ea05f0d0f7b
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-22 10:56:31 +0200

    Add files via upload

--- ConditionalConstructorsInJava.java
+++ ConditionalConstructorsInJava.java
@@ -1,5 +1,15 @@
 public class ConditionalConstructorsInJava {
     public static void main(String[] args) {
 
+     boolean isAuthenticated = true;
+     if (isAuthenticated)
+        System.out.println("LoggedIn");
+
+        // if (!isAuthenticated)
+        else
+        System.out.println("Not Logged In.");
+
+
+
     }
 }
