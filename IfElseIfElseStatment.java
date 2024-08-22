aa314aa33f1eafce0d7ce5cc4c2a39bdffd1271d
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-22 11:39:25 +0200

    Add files via upload

--- IfElseIfElseStatment.java
+++ IfElseIfElseStatment.java
@@ -1,2 +1,24 @@
 public class IfElseIfElseStatment {
+    public static void main(String[] args) {
+
+        float sellingPrice = 1200, costPrice = 1000;
+        if(sellingPrice > costPrice)
+             System.out.println("profit");
+        else if(costPrice > sellingPrice)
+            System.out.println("Loss");
+        else
+            System.out.println("No Profit No Loss");
+
+
+
+        if(sellingPrice > costPrice)
+        System.out.println("profit");
+        else {
+            if (costPrice > sellingPrice)
+                System.out.println("Loss");
+            else
+                System.out.println("No Profit No Loss");
+
+        }
+    }
 }
