6b2e55840c2d8e141d67b21d50c7a6e7b86c80f2
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-08-23 23:34:11 +0200

    Add files via upload

---
+++ JumpStatements.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class JumpStatements {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("quit")) {
+            System.out.println("Enter Message: ");
+            input = scanner.nextLine().toLowerCase();
+
+            if (input.equals("quit"))
+                break;
+            if (input.equals("pass"))
+                continue;
+            System.out.println("Message : " + input);
+        }
+    }
+}
