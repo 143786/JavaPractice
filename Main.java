f3bae74f3e0b94a3ef95ef9664e3c7882f7a4655
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-04 21:49:29 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        // if statement =  performs a block of code if it's condition evaluates to be here
-        int age = 13;
-
-        if(age>= 18){
-            System.out.println("You are an adult!");
-
-        }
-        else if (age >=75) {
-            System.out.println("Ok Tabish!");
-        }
-
-        else if (age >=13) {
-            System.out.println("You are a teenager");
-
-        }
-        else {
-            System.out.println("You are not an adult");
-        }
-
+        String n = "Ayline";
+        int i = 20;   // 32bit   max 2,147,483,647 min -2,147,483,648
+        double d = 20.1;  // 64bit   max 1.7976931348623157×10308   min −1.7976931348623157×10308
+        float f = 2478484.34f; // 32bit  max
+        byte b = 127;     // 8bit    max 127                        min -128
+        long l = 744784947398L;  // 64bit  max 9223372036854775807        min -9223372036854775808
+        boolean bo = true;
+        short sh = 32767;        // 16bit  max 32767                      min -32768
+        char ch = 'a';          // 
+        System.out.println(n);
     }
 }
