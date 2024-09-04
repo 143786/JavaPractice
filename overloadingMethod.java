39084e25561730e33a13bddb9af936330b992738
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-04 12:18:55 +0200

    Add files via upload

--- overloadingMethod.java
+++ overloadingMethod.java
@@ -1,10 +1,8 @@
 public class overloadingMethod {
     public static void main(String[] args) {
-        int newScore = calculateScore("Tabish", 500);
-        System.out.println("New score is " + newScore);
+        System.out.println("New score is " + calculateScore("Tabish ", 500));
+        System.out.println("New score is " + calculateScore(10));
 
-        calculateScore(75);
-        calculateScore("Ayline ",  100);
     }
     public static int calculateScore(String playerName, int score){
         System.out.println("Player " + playerName + "scored " + score + "points");
@@ -13,9 +11,7 @@ public class overloadingMethod {
     }
 
     public static int calculateScore( int score){
-        System.out.println("unnamed player scored " + score + "points");
-        return score * 1000;
-
+       return calculateScore("Anonymous", score);
     }
 
     public static int calculateScore(){
