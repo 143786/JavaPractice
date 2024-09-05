74f4d80c07a403eaa619dc4b49ea02d626bc1b95
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-05 15:09:19 +0200

    Add files via upload

--- Main2.java
+++ Main2.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class Main2 {
     public static void main(String[] args) {
 
+        Tabish();
+
+
+    }
+
+    public static void Tabish() {
         Scanner ayline = new Scanner(System.in);
 
         double fNum, sNum, answer;
@@ -14,12 +20,7 @@ public class Main2 {
         sNum = ayline.nextDouble();
 
         answer = fNum + sNum;
-        
+
         System.out.println(answer);
-
-
-
-
-
     }
 }
