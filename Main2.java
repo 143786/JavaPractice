db330cc8db3df5795eee89d4a7065511e493be95
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-05 14:51:22 +0200

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+
+        Scanner ayline = new Scanner(System.in);
+
+        double fNum, sNum, answer;
+
+        System.out.println("Please enter the first number: ");
+        fNum = ayline.nextDouble();
+        System.out.println("Please enter the second number");
+
+        sNum = ayline.nextDouble();
+
+        answer = fNum + sNum;
+        
+        System.out.println(answer);
+
+
+
+
+
+    }
+}
