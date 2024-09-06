848f4b5286a5975527312e31b70dcc0e4b5bd510
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-06 10:28:32 +0200

    Add files via upload

--- parsClick03/Main.java
+++ parsClick03/Main.java
@@ -1,7 +1,19 @@
 package parsClick03;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your first name ");
+        String temp = input.nextLine();
+
+        NewClass object = new NewClass();
+
+        object.setName(temp);
+        object.saying();
     }
+
+
 }
--- parsClick03/NewClass.java
+++ parsClick03/NewClass.java
@@ -3,8 +3,16 @@ package parsClick03;
 public class NewClass {
     private String girlName;
     public void setName(String name){
+        girlName = name;
 
     }
+    public String getName() {
+        return girlName;
+    }
+    public void saying() {
+        System.out.printf("That's your girl name %s ",getName());
+    }
+
 
 
 }
