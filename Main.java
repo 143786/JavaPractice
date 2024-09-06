1e9940e3632dc0497ae0a41ae2b7afd53ec952d7
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-06 06:24:21 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,14 +1,8 @@
+package parsClick;
+
 public class Main {
     public static void main(String[] args) {
-        String n = "Ayline";
-        int i = 20;   // 32bit   max 2,147,483,647 min -2,147,483,648
-        double d = 20.1;  // 64bit   max 1.7976931348623157×10308   min −1.7976931348623157×10308
-        float f = 2478484.34f; // 32bit  max
-        byte b = 127;     // 8bit    max 127                        min -128
-        long l = 744784947398L;  // 64bit  max 9223372036854775807        min -9223372036854775808
-        boolean bo = true;
-        short sh = 32767;        // 16bit  max 32767                      min -32768
-        char ch = 'a';          // 
-        System.out.println(n);
+        Main01 object = new Main01();
+        object.method();
     }
 }
---
+++ Main01.java
@@ -0,0 +1,7 @@
+package parsClick;
+
+public class Main01  {
+    public void method() {
+        System.out.println("This is a new class");
+    }
+}
