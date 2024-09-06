77c7602ef69deeca27b9f82c8c5c1686aedaa07c
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-06 05:58:49 +0200

    Add files via upload

---
+++ If.java
@@ -0,0 +1,14 @@
+public class If {
+    public static void main(String[] args) {
+
+       int ayline = 0;
+       while(ayline == 10) {
+            System.out.println(ayline);
+            ayline++;
+
+        }
+
+
+
+    }
+}
