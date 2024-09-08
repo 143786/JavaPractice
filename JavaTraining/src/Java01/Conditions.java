587b4b15375adfbdb7d4fec22f2bce74da24689a
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-08 15:10:15 +0200

    Add files via upload

---
+++ JavaTraining/src/Java01/Conditions.java
@@ -0,0 +1,9 @@
+package Java01;
+
+public class Conditions {
+    public static void main(String[] args) {
+        int age = 25;
+        System.out.println(age > 50 ? "You're old" : "You're young");
+
+    }
+}
