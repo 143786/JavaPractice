abbafb24e500ded378b5dac487b1ec24a5623d62
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-09 10:44:18 +0200

    Add files via upload

--- Java01/ArraysTable.java
+++ Java01/ArraysTable.java
@@ -3,7 +3,7 @@ package Java01;
 public class ArraysTable {
     public static void main(String[] args) {
         System.out.println("Index\tValue");
-        int ayline[] = {7676, 32, 18, 12, 54, 2, 45, 76, 87, 100, 130, 4768, 328393, 33333 };
+        int[] ayline = {7676, 32, 18, 12, 54, 2, 45, 76, 87, 100, 130, 4768, 328393, 33333 };
 
         for (int i=0; i<ayline.length; i++) {
             System.out.println(i + "\t\t" + ayline[i]);
--- Java01/SumOfArraysElements.java
+++ Java01/SumOfArraysElements.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class SumOfArraysElements {
     public static void main(String[] args) {
-        int ayline [] = {21, 43, 43, 76, 87, 45};
+        int[] ayline = {21, 43, 43, 76, 87, 45};
         int sum = 0;
 
         for (int i = 0; i < ayline.length; i++) {
