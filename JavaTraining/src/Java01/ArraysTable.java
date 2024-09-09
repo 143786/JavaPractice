aa3b6af617ea30332ac181b7b46d07cdbb33a6cd
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-09-09 09:28:33 +0200

    Add files via upload

---
+++ JavaTraining/src/Java01/ArraysTable.java
@@ -0,0 +1,12 @@
+package Java01;
+
+public class ArraysTable {
+    public static void main(String[] args) {
+        System.out.println("Index\tValue");
+        int ayline[] = {32, 18, 12, 54, 2};
+
+        for (int i=0; i<ayline.length; i++) {
+            System.out.println(i + "\t\t" + ayline[i]);
+        }
+    }
+}
