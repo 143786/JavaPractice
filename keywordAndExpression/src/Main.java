3efa97070f26a2d1e55b7cd45f2445f19e16722e
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-11-20 10:36:38 +0100

    Add files via upload

--- keywordAndExpression/src/Main.java
+++ keywordAndExpression/src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         double kilometers = (100 * 1.609344); /* here we create
-        an expression to convert miles into kilometers. */
+        an expression to convert miles into kilometers. the
+         expression component can include variables, values and
+         operators but NOT type like double here, By including
+          the datatype and ending with a semicolon, we have
+           now created a valid Java statement.
+           ** Expression => kilometers = (100 * 1.609344)
+           ** Statement  => double kilometers = (100 * 1.609344); */
 
-    }
+        int highScore = 50;
+        if (highScore > 25 ) {
+            highScore = 1000 + highScore;   // add bonus points
+        }
+
+        int health = 100;
+
+        if ((health < 25) && (highScore > 1000)) {
+            highScore = highScore - 1000;
+        }
+
+
+    } 
 }
