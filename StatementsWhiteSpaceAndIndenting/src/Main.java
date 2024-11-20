f35cca281f266d8e38c5bf684ac62c06fafd949e
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-11-20 11:11:09 +0100

    Add files via upload

---
+++ StatementsWhiteSpaceAndIndenting/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int myVariable = 50; /* this entire line is a statement.
+java statements can be assignment expressions, like
+there, where we're assigning the value 50 to the variable
+myVariable. */
+
+        myVariable++;
+        myVariable--;   /* these all two are a statements,
+without semicolon , myVariable++ is an expression */
+
+        System.out.println("This is a test"); // this is also a statement
+// the statement can have different line NOT just in one line.
+        System.out.println("This is a second test");
+        System.out.println("this is the 3d test");
+    }
+
+}
