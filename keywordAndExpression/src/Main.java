606edb7a485adefed7bedda692855ecae07151f8
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-11-20 10:22:40 +0100

    Add files via upload

---
+++ keywordAndExpression/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double kilometers = (100 * 1.609344); /* here we create
+        an expression to convert miles into kilometers. */
+
+    }
+}
