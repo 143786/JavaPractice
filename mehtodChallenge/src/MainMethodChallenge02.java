87a8af6068aaaff7e038d7d148530cd05bdce4cf
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-11-22 00:32:09 +0100

    Add files via upload

--- mehtodChallenge/src/MainMethodChallenge02.java
+++ mehtodChallenge/src/MainMethodChallenge02.java
@@ -1,6 +1,4 @@
-import java.util.Timer;
-
-public class Main {
+public class MainMethodChallenge02 {
     public static void main(String[] args) {
 
         int highScorePosition = calculateHighScorePosition(1500);
@@ -30,17 +28,15 @@ public class Main {
 
     public static int calculateHighScorePosition (int playerScore) {
 
+        int position = 4;
         if (playerScore >= 1000 ) {
-            return 1;
-        } else if (playerScore >= 500 && playerScore < 1000 ) {
-            return 2;
-        } else if (playerScore >= 100 && playerScore < 500) {
-            return 3;
-            
-        } else {
-            return 4;
+            position = 1;
+        } else if (playerScore >= 500 ) {
+            position = 2;
+        } else if (playerScore >= 100 ) {
+            position = 3;
+        }
+            return position;
         }
     }
-
-
-}
+    
