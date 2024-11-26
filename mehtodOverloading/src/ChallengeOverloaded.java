6ff70506225cb56de300924a88bc69bee3990e02
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-11-26 22:53:13 +0100

    Add files via upload

---
+++ mehtodOverloading/src/ChallengeOverloaded.java
@@ -0,0 +1,9 @@
+public class ChallengeOverloaded {
+    public static void main(String[] args) {
+        System.out.println("5ft, 8in = " + convertToCentimeters(5, 8) + "cm");
+        System.out.println("68in = " + convertToCentimeters());
+
+    }
+
+   // public static double
+}
