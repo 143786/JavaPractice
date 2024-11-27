0950632ddaa0cbd8aeb66bd72ac65cae1c9c54ba
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-11-27 12:17:14 +0100

    Add files via upload

---
+++ ChallengeOverloadingMethod/src/Main3.java
@@ -0,0 +1,31 @@
+public class Main3 {
+    public static void main(String[] args) {
+
+
+        System.out.println("5ft, 8in = " + convertToCentimeters(5, 8) + "cm");
+        System.out.println("68in = " + convertToCentimeters(68) + "cm");
+
+        }
+
+        public static double convertToCentimeters(int inches) {
+            return inches * 2.54;
+        }
+
+
+        public static double convertToCentimeters(int feet, int inches) {
+             // return convertToCentimeters((feet * 12) + inches );
+            int feetToInches = feet * 12;
+            int totalInches = feetToInches + inches;
+            double result = convertToCentimeters(totalInches);
+            return result;
+        }
+
+
+    }
+
+
+
+    /*
+    we could've written that method differently and set up local
+    variables to do all the same work, here we do that :
+     */
