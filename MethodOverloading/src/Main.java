a539cf0bcbefb2493b09b853f0b4ddb2462c925f
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-11-27 11:26:20 +0100

    Add files via upload

--- MethodOverloading/src/Main.java
+++ MethodOverloading/src/Main.java
@@ -2,15 +2,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-       int  newScore = calculateScore("Tabish", 500);
-        System.out.println("New score is " + newScore);
+        System.out.println("New score is " + calculateScore( "Ayline", 500));
+        System.out.println("New score is " + calculateScore(10));
 
     }
     public static int calculateScore(String playerName, int score ) {
 
-        System.out.println("player " + playerName + " scored " + score + " points ");
+        System.out.println("Player " + playerName + " scored " + score + " points ");
         return score * 1000;
     }
+
+    public static int calculateScore(int score ) {
+
+       return calculateScore("Anonymous", score );
+    }
+    public static int calculateScore() {
+
+        System.out.println("No player name, no player score. ");
+        return 0;
+    }
+
+
 }
 
 
