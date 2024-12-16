e9b2da44e820a00a72b5ddae23bdb2bc13ef6154
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-12-16 15:33:26 +0100

    Add files via upload

--- JavaSpring01/src/ForLoop.java
+++ JavaSpring01/src/ForLoop.java
@@ -4,7 +4,7 @@ public class ForLoop {
         for(int i=1; i<=5; i++) {
             System.out.println("DAY " + i);
             for (int j=1; j<=9; j++ ) {
-                System.out.println("    " + (j+8));
+                System.out.println("    " + (j+8) + " - " + (j+9));
             }
         }
     }
