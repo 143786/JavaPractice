afbd9096ad7ffd013460718027d381205741cab1
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2025-02-24 23:12:02 +0100

    Add files via upload

---
+++ ClassesPart1/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car = new Car();
+       // car.make = "Porsche";
+       //  car.model = "Carrera";
+      //  car.color = "Red";
+
+        /*
+       As I defined these fields as private on the
+       car class, and because of that, I can't set
+       tha values on any car object I create this
+       way.    */
+
+        car.describeCar();
+
+
+    }
+}
